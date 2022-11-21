2b782d29c4488280d97f01fe46897d6270871e6d
marcocalmetti@gmail.com <marcocalmetti@gmail.com>
2022-11-21 20:32:11 +0100

    Company Controller Adjuct

--- CompanyProject/Controllers/CompanyController.cs
+++ CompanyProject/Controllers/CompanyController.cs
@@ -30,18 +30,34 @@ namespace CompanyProject.Controllers
             
         }
 
-        public async static Task<int> checkReseller()
+        public async static Task<string> checkReseller(string BusinessName, string VAT, string Address, string City, string PostalCode, string Mail, string TelephoneNumber)
         {
             using (CompanyContext context = new CompanyContext())
             {
-                return await context.Resellers
-                    .Select(s => s)
-                    .Where(s => BusinessName != null ? s.BusinessName.Contains(BusinessName) : true)
-                    .Where(s => VAT != null ? s.VAT.Contains(VAT) : true)
-                    .Where(s => City != null ? s.City == City : true)
-                    .Count();
-                    
+                try
+                {
+                    if (await context.Resellers.Select(s => s).Where(s => BusinessName != null ? s.BusinessName.Contains(BusinessName) : true).CountAsync() > 0)
+                        return "There is already another company name with the same name";
+                
+                    if(await context.Resellers.Select(s => s).Where(s => BusinessName != null ? s.BusinessName.Contains(BusinessName) : true).CountAsync() > 0)
+                        return "There is already another company with the same VAT";
+
+                    if (await context.Resellers.Select(s => s).Where(s => BusinessName != null ? s.BusinessName.Contains(BusinessName) : true).CountAsync() > 0)
+                        return "There is already another company with the same VAT";
+
+                    return null;
+                }
+                catch(Exception e)
+                {
+                    throw;
+                }
+                
             }
+        }
+
+        public static bool EmailFormatChecker(string email)
+        {
+            if(email.Contains("@") && email.Contains(".") && email.Substring(0,email.IndexOf("@")).Length > 0 && )
         }
     }
 }
