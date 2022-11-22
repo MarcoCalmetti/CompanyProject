6ae67d8c5da3144debfc1edf8579874c83739ab4
AlleAmato <alleamato13@gmail.com>
2022-11-22 11:03:19 +0100

    aggiornamento views Amato2

--- CompanyProject/Views/MainWindow.xaml.cs
+++ CompanyProject/Views/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +8,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 
 namespace CompanyProject
 {
