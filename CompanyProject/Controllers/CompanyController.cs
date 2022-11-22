8f372622b5a90d6e0095de2b2a7924b1c938ee57
MarcoCalmetti <marcocalmetti@gmail.com>
2022-11-22 11:20:40 +0100

    CompanyContoller -> CheckReseller

--- CompanyProject/Controllers/CompanyController.cs
+++ CompanyProject/Controllers/CompanyController.cs
@@ -27,29 +27,56 @@ namespace CompanyProject.Controllers
 
         public async static Task AddReseller(string BusinessName, string VAT, string Address, string City, string PostalCode, string Mail, string TelephoneNumber)
         {
-            
+            using (CompanyContext context = new CompanyContext())
+            {
+                try
+                {
+                    checkReseller(BusinessName, VAT, Mail, TelephoneNumber);
+                    context.Resellers.Add(
+                        new Reseller
+                        {
+                            BusinessName = BusinessName,
+                            VAT = VAT,
+                            Address = Address,
+                            City = City,
+                            PostalCode = PostalCode,
+                            Mail = Mail,
+                            TelephoneNumber = TelephoneNumber
+                        });
+                }catch(ArgumentException e)
+                {
+                    throw e;        
+                }
+            }
         }
 
-        public async static Task<string> checkReseller(string BusinessName, string VAT, string Address, string City, string PostalCode, string Mail, string TelephoneNumber)
+        public async static Task checkReseller(string BusinessName, string VAT, string Mail, string TelephoneNumber)
         {
             using (CompanyContext context = new CompanyContext())
             {
                 try
                 {
                     if (await context.Resellers.Select(s => s).Where(s => BusinessName != null ? s.BusinessName.Contains(BusinessName) : true).CountAsync() > 0)
-                        return "There is already another company name with the same name";
+                        throw new ArgumentException "There is already another company  with the same Business name";
                 
-                    if(await context.Resellers.Select(s => s).Where(s => BusinessName != null ? s.BusinessName.Contains(BusinessName) : true).CountAsync() > 0)
-                        return "There is already another company with the same VAT";
+                    if(await context.Resellers.Select(s => s).Where(s => VAT != null ? s.BusinessName.Contains(VAT) : true).CountAsync() > 0)
+                        throw new ArgumentException "There is already another company with the same VAT";
 
-                    if (await context.Resellers.Select(s => s).Where(s => BusinessName != null ? s.BusinessName.Contains(BusinessName) : true).CountAsync() > 0)
-                        return "There is already another company with the same VAT";
+                    if(!TelephoneNumber.All(char.IsDigit))
+                        throw new ArgumentException "The telephone format is not correct";
 
-                    return null;
+                    if(!EmailFormatChecker(email))
+                        throw new ArgumentException "The email format is not correct";
+                    
+                    if (await context.Resellers.Select(s => s).Where(s => Mail != null ? s.BusinessName.Contains(Mail) : true).CountAsync() > 0)
+                        throw new ArgumentException "There is already another company with the same Mail";
+
+                    if (await context.Resellers.Select(s => s).Where(s => TelephoneNumber != null ? s.BusinessName.Contains(TelephoneNumber) : true).CountAsync() > 0)
+                        throw new ArgumentException "There is already another company with the Telephone Number ";
                 }
-                catch(Exception e)
+                catch(ArgumentException e)
                 {
-                    throw;
+                    throw e;
                 }
                 
             }
@@ -57,7 +84,11 @@ namespace CompanyProject.Controllers
 
         public static bool EmailFormatChecker(string email)
         {
-            if(email.Contains("@") && email.Contains(".") && email.Substring(0,email.IndexOf("@")).Length > 0 && )
+            if (email.Contains("@") && email.Substring(0, email.IndexOf("@")).Length > 0 && email.Substring(email.IndexOf("@"), email.Length - 1).Contains("."))
+                return true;
+            return false
         }
+
+
     }
 }
