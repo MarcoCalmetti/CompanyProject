f4c1622596dc98fcf33cb8035609ec1fa01f407a
Florim Ramadani <florimrama31@gmail.com>
2022-11-29 12:33:55 +0100

    AddEditOrderViewModel Finito

--- CompanyProject/ViewModels/AddEditOrderViewModel.cs
+++ CompanyProject/ViewModels/AddEditOrderViewModel.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CompanyProject.ViewModels;
 using CompanyProject.Models; 
+
 
 namespace CompanyProject.ViewModels
 {
-    class AddEditOrderViewModel
+    class AddEditOrderViewModel : BaseViewModel
     {
+        #region Properties
+        private List<Item> itemlist;
+
+        public List<Item> ItemList
+        {
+            get { return itemlist; }
+            set { itemlist = value; NotifyPropertyChanged("ItemList"); }
+        }
+
+        private Item selecteditem;
+
+        public Item SelectedItem
+        {
+            get { return selecteditem; }
+            set { selecteditem = value; NotifyPropertyChanged("SelectedItem"); }
+        }
+        #endregion
+
+        #region Constructor
+        public AddEditOrderViewModel()
+        {
+
+        }
+        #endregion
+
+        #region Methods
+
+
+
+         
+        #endregion
     }
 }
