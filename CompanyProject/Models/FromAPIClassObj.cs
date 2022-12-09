4777b4baea4bc59b9a4de72591eaf6b937763911
MarcoCalmetti <marcocalmetti@gmail.com>
2022-12-09 14:23:24 +0100

    Correzione Errori

--- CompanyProject/Models/FromAPIClassObj.cs
+++ CompanyProject/Models/FromAPIClassObj.cs
@@ -19,11 +19,11 @@ namespace CompanyProject.Models
         public string Id { get; set; }
         public string ResellerId { get; set; }
         public int CustomerId { get; set; }
-        public DateTime OrderDate { get; set; }
+        public DateTime? OrderDate { get; set; }
         public int OrderStateId { get; set; }
-        public DateTime SendDate { get; set; }
-        public DateTime StartProductionDate { get; set; }
-        public DateTime StopProductionDate { get; set; }
+        public DateTime? SendDate { get; set; }
+        public DateTime? StartProductionDate { get; set; }
+        public DateTime? StopProductionDate { get; set; }
         public string Notes { get; set; }
         public List<OrderItem> OrderItems { get; set; }
     }
