f3109d69646c0b0b68ff75c3393e5c1ba2bba039
AlleAmato <alleamato13@gmail.com>
2022-12-14 09:56:38 +0100

    implementazione idataerrorinfo nelle classi estese

--- CompanyProject/Models/OrderRowExtension.cs
+++ CompanyProject/Models/OrderRowExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -9,8 +10,32 @@ using System.Threading.Tasks;
 namespace CompanyProject.Models
 {
     [MetadataTypeAttribute(typeof(OrderRowMetaData))]
-    public partial class OrderRowExtension
+    public partial class OrderRowExtension : IDataErrorInfo
     {
+        public string this[string columnName]
+        {
+            get
+            {
+                ValidationContext valContext = new ValidationContext(this)
+                {
+                    MemberName = columnName
+                };
+
+                List<ValidationResult> validationResults = new List<ValidationResult>();
+
+                if (Validator.TryValidateProperty(
+                        GetType().GetProperty(columnName).GetValue(this),
+                        valContext,
+                        validationResults))
+                    return "";
+
+                return validationResults.First().ErrorMessage;
+            }
+        }
+
+        private string _error;
+        public string Error => _error;
+
     }
 
     public class OrderRowMetaData
--- CompanyProject/Models/ResellerExtension.cs
+++ CompanyProject/Models/ResellerExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -9,15 +10,38 @@ using System.Threading.Tasks;
 namespace CompanyProject.Models
 {
     [MetadataTypeAttribute(typeof(ResellerMetaData))]
-    public partial class Reseller
+    public partial class Reseller : IDataErrorInfo
     {
+        public string this[string columnName]
+        {
+            get
+            {
+                ValidationContext valContext = new ValidationContext(this)
+                {
+                    MemberName = columnName
+                };
+
+                List<ValidationResult> validationResults = new List<ValidationResult>();
+
+                if (Validator.TryValidateProperty(
+                        GetType().GetProperty(columnName).GetValue(this),
+                        valContext,
+                        validationResults))
+                    return "";
+
+                return validationResults.First().ErrorMessage;
+            }
+        }
+
+        private string _error;
+        public string Error => _error;
+
     }
 
     public class ResellerMetaData
     {
         public int ResellerID { get; set; }
         [Required(ErrorMessage = "Campo obbligatorio")]
-        //[Range(0,float.MaxValue,ErrorMessage ="Importo negativo")]??
         public string BusinessName { get; set; }
         [Required(ErrorMessage = "Campo obbligatorio")]
         public string VAT { get; set; }
